d48bfaadb096d0598e225b0d7a983056ef25379e
yeongjin <hanyj0104@naver.com>
2023-11-21 00:05:14 +0900

    feat: bookmark추가로 entity 연관관계 추가

--- src/main/java/com/ssafy/homer/user/domain/User.java
+++ src/main/java/com/ssafy/homer/user/domain/User.java
@@ -1,17 +1,17 @@
 package com.ssafy.homer.user.domain;
 
+import com.ssafy.homer.bookmark.domain.Bookmark;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+
 @Entity
 @Getter
 @Setter
@@ -36,5 +36,11 @@ public class User{
     private String role;
 
     private String userPhoto;
+
+
+    @OneToMany(mappedBy ="user")
+    private List<Bookmark> bookmarkList;
+
+
 
 }
