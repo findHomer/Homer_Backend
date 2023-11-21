5954f12ab49b04bddb2cb7c58fdcc0c5f63d8524
Baek Seungyun <fangdol888@naver.com>
2023-11-21 12:13:52 +0900

    refactor : redis 관련 설정 수정

--- src/main/java/com/ssafy/homer/chat/model/dto/ChatMessage.java
+++ src/main/java/com/ssafy/homer/chat/model/dto/ChatMessage.java
@@ -1,11 +1,18 @@
 package com.ssafy.homer.chat.model.dto;
 
+import java.io.Serializable;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class ChatMessage {
+public class ChatMessage implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
 	public enum MessageType{
 		ENTER, TALK
 	}
