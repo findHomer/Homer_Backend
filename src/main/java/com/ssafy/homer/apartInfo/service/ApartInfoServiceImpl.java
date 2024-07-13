de6f6bf083b04e31df0bcc56d275ef29ce73da50
yeongjin <hanyj0104@naver.com>
2024-07-13 19:15:34 +0900

    refactor: 아파트 상세정보(거래내역) 연산 비동기방식 @Async적용 ApplicationContext의 빈호출하여 내부 메소드 AOP적용 추가

--- src/main/java/com/ssafy/homer/apartInfo/service/ApartInfoServiceImpl.java
+++ src/main/java/com/ssafy/homer/apartInfo/service/ApartInfoServiceImpl.java
@@ -4,8 +4,6 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 import com.ssafy.homer.apartInfo.domain.ApartDeal;
@@ -14,6 +12,9 @@ import com.ssafy.homer.apartInfo.dto.*;
 import com.ssafy.homer.exception.BaseException;
 import com.ssafy.homer.exception.ErrorCode;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -30,6 +31,10 @@ import javax.transaction.Transactional;
 public class ApartInfoServiceImpl implements ApartInfoService{
 	
 	private final ApartInfoRepository apartInfoRepository;
+	private final ApplicationContext context;
+
+	private static final Logger logger = LoggerFactory.getLogger(ApartInfoService.class);
+
 
 	@Override
 	public List<ApartInfoDto> findTotalApart() {
@@ -50,7 +55,7 @@ public class ApartInfoServiceImpl implements ApartInfoService{
 	@Override
 	public ApartInfoDetailDto findApartDetail(String apartId) {
 		 ApartInfo apartInfo =  apartInfoRepository.findById(apartId).orElseThrow(() -> new BaseException(ErrorCode.APART_NOT_FOUND));
-
+		 System.out.println(apartInfo.getParkPerHouse());
 		 //전용면적을 key로 apartDealDto를 넣어줌
 		TreeMap<Float,ArrayList<ApartDealDto>> apartTransactionInfo = new TreeMap<>();
 
@@ -67,14 +72,23 @@ public class ApartInfoServiceImpl implements ApartInfoService{
 		int threadPoolSize = Math.max(2,processors);
 		ExecutorService customThreadPool = Executors.newFixedThreadPool(threadPoolSize);
 		 **/
+
+		ApartInfoService proxy = context.getBean(ApartInfoService.class);
 		// 비동기 작업 리스트
 		List<CompletableFuture<Void>> futures = apartTransactionInfo.entrySet().stream()
-						.map(entry -> aSyncCalcApartDealList(entry, apartDealAreaDtoList))
+						.map(entry -> {
+							try {
+								return proxy.aSyncCalcApartDealList(entry, apartDealAreaDtoList);
+							} catch (InterruptedException e) {
+								throw new RuntimeException(e);
+							}
+						})
 						.collect(Collectors.toList());
 
 		CompletableFuture<Void> allOf = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
 		allOf.join();  // 모든 작업이 완료될 때까지 기다림
 
+		apartDealAreaDtoList.sort((o1,o2)-> (int)(o1.getExclusiveArea()-o2.getExclusiveArea()));
 
 		ApartInfoDetailDto apartInfoDetailDto = ApartInfoDetailDto.builder()
 				 .aptId(apartInfo.getAptId())
@@ -130,7 +144,8 @@ public class ApartInfoServiceImpl implements ApartInfoService{
 	}
 
 	@Async("taskExecutor")
-	public CompletableFuture<Void> aSyncCalcApartDealList(Map.Entry<Float,ArrayList<ApartDealDto>> e, List<ApartDealAreaDto> apartDealAreaDtoList){
+	public CompletableFuture<Void> aSyncCalcApartDealList(Map.Entry<Float,ArrayList<ApartDealDto>> e, List<ApartDealAreaDto> apartDealAreaDtoList) throws InterruptedException {
+		logger.info("Async method started. Thread: " + Thread.currentThread().getName());
 		LocalDate threeYearsAgo = LocalDate.now().minusYears(3);
 
 		Map<String, MonthlyData> monthlyDataMap = new HashMap<>();
@@ -158,6 +173,8 @@ public class ApartInfoServiceImpl implements ApartInfoService{
 		}
 		apartDealAreaDtoList.add(new ApartDealAreaDto(e.getKey(),e.getValue(),averageMonthDtoList));
 		//System.out.println("hello");
+		logger.info("Async method finished. Thread: " + Thread.currentThread().getName());
+
 		return CompletableFuture.completedFuture(null);
 	}
 
